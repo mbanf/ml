66c5256309a19904b208fbeb6081f50475050f7a
Michael Banf <m.banf@fabforce.com>
2022-04-18 15:12:39 +0200

    first commit

---
+++ dcgan_training.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn 
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms 
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from wgan_model import Discriminator, Generator, initialize_weights
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+Z_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
+        )
+    ]
+)
+
+dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms, download=True)
+gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
+initialize_weights(gen)
+initialize_weights(disc)
+
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
+
+fixed_noise = SummaryWriter(f"logs/real")
+writer_real = SummaryWriter(f"logs/fake")
+step = 0
+
+gen.train()
+disc.train()
+
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (real, _) in enumerate(loader):
+        real = real.to(device)
+        noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1)).to(device)
+        fake = gen(noise)
+        
+        disc_real = disc(real).reshape(-1)
+        loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = disc(fake).reshape(-1)
+        loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        disc.zero_grad()
+        loss_disc.backward(retain_graph=True)
+        opt_disc.step()
+
+        output = disc(fake).reshape(-1)
+        loss_gen = criterion(output)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+
+
